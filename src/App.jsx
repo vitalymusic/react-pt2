83eed3df5f67254aa4fcd8237b3aaa7d79420d43
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-10-07 19:11:00 +0300

    FullCart Ver2

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,24 @@
 import Header from './Header';
 import Footer from './Footer';
 import Card from './Card';
-
+import { useState } from "react";
+import FullScreenPhoto from './FullScreenPhoto';
 
 
 import './App.css';
 
 function App() {
+
+  const [visible, setVisible] = useState(false);
+  const [src, setSrc] = useState("");
+
+  function openCard(el){
+    setSrc(el.src);
+    setVisible(!visible);
+}
+
+
+
   return (
     <div className="App">
       <Header />
@@ -15,16 +27,20 @@ function App() {
       <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero quo error amet officiis, similique, eos nostrum, repellat in hic eveniet minima voluptatem? Tempore libero explicabo ab sequi ad ipsam temporibus.</p>
       {/* <img className="image" src="https://picsum.photos/200/300" alt="picsum" /> */}
       <div className="cards flex">
-        <Card src="https://picsum.photos/id/5/200/300" title="Bilde 1" />
-        <Card src="https://picsum.photos/id/9/200/300" title="Bilde 2" />
-        <Card src="https://picsum.photos/id/10/200/300" title="Bilde 3" />
-        <Card src="https://picsum.photos/id/20/200/300" title="Bilde 4" />
-        <Card src="https://picsum.photos/id/20/200/300" title="Bilde 4" />
-        <Card src="https://picsum.photos/id/20/200/300" title="Bilde 4" />
-        <Card src="https://picsum.photos/id/20/200/300" title="Bilde 4" />
-        <Card src="https://picsum.photos/id/20/200/300" title="Bilde 4" />
-        <Card src="https://picsum.photos/id/20/200/300" title="Bilde 4" />
+        <Card src="https://picsum.photos/id/5/200/300" title="Bilde 1" onClick={openCard(this)} />
+        <Card src="https://picsum.photos/id/9/200/300" title="Bilde 2" onClick={openCard(this)}/>
+        <Card src="https://picsum.photos/id/10/200/300" title="Bilde 3" onClick={openCard(this)}/>
+        <Card src="https://picsum.photos/id/20/200/300" title="Bilde 4" onClick={openCard(this)}/>
+        <Card src="https://picsum.photos/id/20/200/300" title="Bilde 4" onClick={openCard(this)}/>
+        <Card src="https://picsum.photos/id/20/200/300" title="Bilde 4" onClick={openCard(this)}/>
+        <Card src="https://picsum.photos/id/20/200/300" title="Bilde 4" onClick={openCard(this)}/>
+        <Card src="https://picsum.photos/id/20/200/300" title="Bilde 4" onClick={openCard(this)}/>
+        <Card src="https://picsum.photos/id/20/200/300" title="Bilde 4" onClick={openCard(this)}/>
       </div>
+
+      {visible&&
+              <FullScreenPhoto src={src} />
+       }
       <Footer />
     </div>
   );
--- src/Card.jsx
+++ src/Card.jsx
@@ -5,23 +5,16 @@ import FullScreenPhoto from './FullScreenPhoto';
 
 export default function Card(props) {
 
-  const [visible, setVisible] = useState(false);
-  const [src, setSrc] = useState("");
-
-  function openCard(){
-    setVisible(!visible);
-}
+ 
 
   return (
     <>
-    <div className="card" onClick={openCard}>
+    <div className="card">
         <img src={props.src} alt={props.title} />
         <h5>{props.title}</h5>
         
     </div>
-    {visible&&
-              <FullScreenPhoto src={src} />
-        }
+    
   </>
   )
 }
