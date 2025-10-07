ea455b813cbdbd9283af93fc6860eafee2de23ee
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-10-07 18:35:07 +0300

    useState

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,27 @@
 import './Card.css';
+import { useState } from "react";
+
 
 export default function Card(props) {
+
+  const [visible, setVisible] = useState(false);
+  
+  function openCard(){
+    setVisible(!visible);
+}
+
+
+
   return (
-    <div className="card">
+    <div className="card" onClick={openCard}>
         <img src={props.src} alt={props.title} />
         <h5>{props.title}</h5>
+        {visible&&
+            <>
+              <div className='alert'>Parādās bloks</div>
+            </>
+          
+        }
     </div>
   )
 }
